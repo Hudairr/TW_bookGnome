d6db60b955c443afd398cc1099b66e585bec5203
Huda Imran R <hudaimranofficial@gmail.com>
2024-06-08 00:59:14 +0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,8 @@ import cors from "cors"
 import bookRoute from "../backend/route/book.route.js"
 import userRoute from "../backend/route/user.route.js"
 
+dotenv.config(); // Load environment variables from .env file
+
 const app = express()
 
 app.use(cors(
@@ -16,8 +18,6 @@ app.use(cors(
     }
 ));
 app.use(express.json());
-
-dotenv.config();
 
 const PORT=process.env.PORT || 4000;
 const URI=process.env.MongoDBURI;
