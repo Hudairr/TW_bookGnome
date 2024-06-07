d319fd4327621d38cd0c233b0534c8baa6d5304d
Huda Imran R <hudaimranofficial@gmail.com>
2024-06-08 00:48:01 +0400

    Update Signup.jsx

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -19,7 +19,7 @@ function Signup() {
         email:data.email,
         password:data.password
       }
-      await axios.post("http://localhost:4001/user/signup", userInfo)
+      await axios.post("https://tw-book-gnome.vercel.app/user/signup", userInfo)
       .then((res)=>{
         console.log(res.data)
         if (res.data) {
